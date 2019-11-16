44d37aae1108181694ad35b98dfe4d287f5fa991
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-11-16 19:40:54 +0900

    add shift

--- caesar.hs
+++ caesar.hs
@@ -5,3 +5,7 @@ let2int c = ord c - ord 'a'
 
 int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
+
+shift :: Int -> Char -> Char
+shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
+          | otherwise = c
